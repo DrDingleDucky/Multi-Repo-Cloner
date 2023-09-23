2b92740adbdafa3903b90c751cbf70a02e582b4b
George Davidson <georgedavidson392@gmail.com>
2023-09-23 15:39:19 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Mult Repo Cloner
+
+Clone all repositories under a user's name.
+
+## Python Version
+
+This project was developed using Python 3.11.3.
+
+## Install Dependencies
+
+Install the necessary dependencies.
+
+```text
+pip install -r requirements.txt
+```
+
+## How To Use
+
+Run `main.py` and enter a user's GitHub username.
---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+import requests
+
+
+def clone_repos(username):
+    url = f"https://api.github.com/users/{username}/repos"
+    response = requests.get(url)
+    repos = response.json()
+
+    for number, repo in enumerate(repos):
+        repo_name = repo['name']
+        repo_url = repo['clone_url']
+        repo_path = os.path.join("cloned_repos", repo_name)
+
+        subprocess.run(['git', 'clone', repo_url, repo_path])
+        print("-" * 50 + f" {number + 1} : cloned '{repo_name}' successfully")
+
+    print("cloning complete")
+
+
+username = input("username > ")
+clone_repos(username)
